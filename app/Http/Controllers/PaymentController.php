61721d7ae5a1e6427ee24cc5f170926f41e8d2f4
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-07 15:20:19 +0700

    update some

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Storage;
 
 class PaymentController extends Controller
 {
@@ -13,7 +15,7 @@ class PaymentController extends Controller
         $token = '0D/3uMcC1QWZrqO+0OTRzTRqa5ZidIIrFl3l/Boo+3fR25DbS3FOW/NSzn+SfvXrCLEFiyDeaOB2fv/DzXdXDzC/jDFNSfI/76DPRO3ROIRq+uf4+ly9iFiC3Vo/8mIrGpBZJbg/l3HQzsk4yG/0znMfLGMB2Zpu2ppcm7CP42LJOZSa';
         $tokenKey = rawurlencode($token);      
         $referenceNo = uniqid();
-        $backgroundUrl = URL::to('redirect/');
+        $backgroundUrl = 'https://fullstacktrainingclass.com/redirect';
         $amount = 0.01;
         print($backgroundUrl);
         $field = 'token='.$tokenKey.'&referenceNo='.$referenceNo.'&backgroundUrl='.$backgroundUrl.'&amount='.$amount.'';
@@ -44,7 +46,27 @@ class PaymentController extends Controller
     }
     public function redirect(Request $request)
     {
-        dd($request);
-        return view('redirect');
+        $transaction = new Transaction();
+        $transaction->name = 'JO';
+        $transaction->save(); 
+        // dd($request->all());
+        // Storage::put('my_result.txt',$request->all());
+        // $respFile = fopen("resp-log.txt", "w") or die("Unable to open file!");
+
+        // $json_str = file_get_contents('php://input');
+        // fwrite($respFile, $json_str . "\n\n");
+
+        // $json_obj = json_decode($json_str);
+
+        // fwrite($respFile, "resultCode=" . 'hello' . "\n");
+
+        // fwrite($respFile, "resultCode=" . $json_obj->resultCode . "\n");
+        // fwrite($respFile, "amount=" . $json_obj->amount . "\n");
+        // fwrite($respFile, "referenceNo=" . $json_obj->referenceNo . "\n");
+        // fwrite($respFile, "gbpReferenceNo=" . $json_obj->gbpReferenceNo . "\n");
+        // fwrite($respFile, "currencyCode=" . $json_obj->currencyCode . "\n");
+        
+        // fclose($respFile);
+        // return view('redirect');
     }
 }
