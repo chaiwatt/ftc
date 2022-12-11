0952c67deb5a0cf877d2fe16265d5ecb5baf779b
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-11 12:54:17 +0700

    update some

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -10,99 +10,11 @@ use Symfony\Component\HttpFoundation\Response;
 class WebHookController extends Controller
 {
     public function webhook(Request $request){
-        $data = '{
-            "object": "event",
-            "id": "evnt_test_56vp5pfwjf1fqjn4ryy",
-            "livemode": false,
-            "location": "/events/evnt_test_56vp5pfwjf1fqjn4ryy",
-            "key": "charge.create",
-            "created": "2017-02-03T13:52:54Z",
-            "data": {
-              "object": "charge",
-              "id": "chrg_test_56vp5pdrw28e2sgyr51",
-              "livemode": false,
-              "location": "/charges/chrg_test_56vp5pdrw28e2sgyr51",
-              "amount": 100000,
-              "currency": "jpy",
-              "description": "Charge for order 3947",
-              "status": "successful",
-              "capture": true,
-              "authorized": true,
-              "reversed": false,
-              "paid": true,
-              "transaction": "trxn_test_56vp5pfhp4tv1o8xp85",
-              "source_of_fund": "card",
-              "refunded": 0,
-              "refunds": {
-                "object": "list",
-                "from": "1970-01-01T00:00:00+00:00",
-                "to": "2017-02-03T13:52:54+00:00",
-                "offset": 0,
-                "limit": 20,
-                "total": 0,
-                "order": null,
-                "location": "/charges/chrg_test_56vp5pdrw28e2sgyr51/refunds",
-                "data": [
-          
-                ]
-              },
-              "return_uri": "http://www.example.com/orders/3947/complete",
-              "offsite": null,
-              "reference": "paym_test_56vp5pe0wpv1c02a2vg",
-              "authorize_uri": "https://api.omise.co/payments/paym_test_56vp5pe0wpv1c02a2vg/authorize",
-              "failure_code": null,
-              "failure_message": null,
-              "card": {
-                "object": "card",
-                "id": "card_test_56vp5nkjti9mi10tmlx",
-                "livemode": false,
-                "country": "us",
-                "city": "Bangkok",
-                "postal_code": "10320",
-                "financing": "",
-                "bank": "",
-                "last_digits": "4242",
-                "brand": "Visa",
-                "expiration_month": 2,
-                "expiration_year": 2019,
-                "fingerprint": "uqEgwbY6J9JcS3z/H1/eDzZmxXacMWo2gT09m+kj//0=",
-                "name": "JOHN DOE",
-                "security_code_check": true,
-                "created": "2017-02-03T13:52:45Z"
-              },
-              "customer": null,
-              "ip": null,
-              "dispute": null,
-              "created": "2017-02-03T13:52:53Z"
-            }
-          }';
-
-        // $payload = json_decode($request->getContent());
-        
-        // $payload = $request->all();// //json_decode($request->getContent(),JSON_PRETTY_PRINT);
         $payload = json_decode($request->getContent(),JSON_PRETTY_PRINT);
-        // $payload_id = $payload['id'];
-
-        // $result = [
-        //     "id" => $payload['id'],
-        //     "object" => $payload['data']['object'],
-        //     // "object_id" => $payload['data']['id'],
-        //     // "title" => $payload['data']['status'],
-        //     // 'amount' => $payload['data']['amount'],
-        //     // 'transaction' => $payload['data']['transaction'],
-        //     // 'source_of_fund' => $payload['data']['source_of_fund'],
-        //     // 'paid' => $payload['data']['paid'],
-        //     // 'created' => $payload['data']['created'],  
-        // ];
-        if ($payload['data']['status'] != 'pending'){
-          Transaction::where('charge_id',$payload['data']['id'])->where('source_id',$payload['data']['source']['id'])->update([
-            'status' => $payload['data']['status']
+        if (trim($payload['data']['status']) != 'pending'){
+          Transaction::where('charge_id',trim($payload['data']['id']))->where('source_id',trim($payload['data']['source']['id']))->update([
+            'status' => trim($payload['data']['status'])
           ]);
         }
-
-        // $transaction->event_id = $payload['id'];
-        // $transaction->event_key = $payload['key'];
-        // $transaction->save();
-
     }
 }
