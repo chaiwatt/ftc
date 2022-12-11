38789ffb6e0edef9e39fa5f670f55e3048f4a3a1
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-11 13:31:02 +0700

    update some

--- resources/views/mail/index.blade.php
+++ resources/views/mail/index.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <h1>{{ $mailData['title'] }}</h1>
-    <p>{{ $mailData['body'] }}</p>
+    <p>{{ $mailData['message'] }}</p>
   
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
