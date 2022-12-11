365437ff47a90e826ab74a1f8564ee3c1fc157c8
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-11 10:43:22 +0700

    update some

--- database/migrations/2022_12_07_081022_create_transactions_table.php
+++ database/migrations/2022_12_07_081022_create_transactions_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->string('event_id');
-            // $table->string('event_key');
+            $table->string('event_key');
             $table->timestamps();
         });
     }
