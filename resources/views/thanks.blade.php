4cc3f96784b9366219f927dd574543c871951423
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-12 08:20:51 +0700

    update some

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -3,7 +3,7 @@
 <section class="section-image section-full-width light align-center " style="background-image:url({{asset('assets/images/bg.svg')}}); margin-bottom:0px">
     <div class="container" style="margin:0;padding-bottom:20px">       
         <h2 data-anima="fade-in" data-time="1000">
-            รายการสั่งซื้อสำเร็จ
+            ข้อมูลและรายละเอียดการสั่งซื้อ
         </h2>
     </div>
 </section>
@@ -13,11 +13,37 @@
     <div class="container">
         <div class="cnt-box cnt-call">
             <div class="caption">
-                <h2>Download the app now<span class="dot">.</span></h2>
-                <p>
-                    Lorem ipsum dolor sitamet consectetur adipisicing elito sed do eiusmod tempore.
-                    Duisera aute irure dolor in reprehenderit e euostrud exercita fugiat nulla pariatur.
-                </p>
+                <ul class="text-list text-list-side boxed-area">
+                    <li>
+                        <h3>Software Engineer</h3>
+                        <p>
+                            Placeat orci commodo, amet quo rem architecto possimus, accumsa.
+                        </p>
+                        <div>Dublin, Ireland</div>
+                    </li>
+                    <li>
+                        <h3>Community Manager</h3>
+                        <p>
+                            Placeat orci commodo, amet quo exercitation ullamco labori rem architecto possimus, accumsa.
+                        </p>
+                        <div>Dublin, Ireland</div>
+                    </li>
+                    <li>
+                        <h3>Sales supervisor</h3>
+                        <p>
+                            Placeat orci commodo, amet quo rem architecto possimus exercitation ullamco accumsa.
+                        </p>
+                        <div>Dublin, Ireland</div>
+                    </li>
+                    <li>
+                        <h3>React Developer</h3>
+                        <p>
+                            Placeat orci commodo, amet quo rem architecto possimus, accumsa.
+                        </p>
+                        <div>Dublin, Ireland</div>
+                    </li>
+                </ul>
+
             </div>
         </div>
     </div>
