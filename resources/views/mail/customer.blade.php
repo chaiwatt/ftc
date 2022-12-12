8560aff9d77687ba2e7a90b10727a21526cd87cb
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-12 15:04:19 +0700

    update some

--- resources/views/mail/customer.blade.php
+++ resources/views/mail/customer.blade.php
@@ -7,13 +7,13 @@
     <title>Notification</title>
 </head>
 <body>
-    <h1>{{ $mailData['title'] }}</h1>
-    <p>{{ $mailData['message'] }}</p>
+    {{-- <h1>{{ $mailData['title'] }}</h1>
+    <p>{{ $mailData['message'] }}</p> --}}
+    {{-- <hr> --}}
     <div>
         <span class="text-sm text-grey-m2 align-middle">ลูกค้า:</span>
         <span class="text-600 text-110 text-blue align-middle">ชัยวัฒน์ ทวีจันทร์</span>
     </div>
-    <hr>
     <div class="text-grey-m2">
         <div>
             ที่อยู่: 105 หมู่ 8 ต.เหมืองง่า อ.เมือง จ.ลำพูน 51000
@@ -21,30 +21,36 @@
         <div>
             อีเมล: joerocknpc@gmail.com
         </div>
-        <div><b>088-2514838</b></div>
+        <div>โทรศัพท์<b>088-2514838</b></div>
         <hr>
         <table>
             <th>
                 <tr>
-                    <td>รายการ</td>
-                    <td>จำนวน</td>
-                    <td>ราคา</td>
+                    <td style="width:500px"><b>รายการ</b></td>
+                    <td style="width:200px"><b>จำนวน</b></td>
+                    <td style="width:200px"><b>ราคา</b></td>
                 </tr>
             </th>
             <tbody>
                 <tr>
-                    <td>อบรมคอร์ส Full-Stack developper</td>
-                    <td>2</td>
-                    <td>3000</td>
+                    <td style="width:500px">อบรมคอร์ส Full-Stack developper</td>
+                    <td style="width:200px">2</td>
+                    <td style="width:200px">3000</td>
                 </tr>
                 <tr>
-                    <td>โดเมน</td>
-                    <td>1</td>
-                    <td>0</td>
+                    <td style="width:500px">โดเมน</td>
+                    <td style="width:200px">1</td>
+                    <td style="width:200px">0</td>
                 </tr>
             </tbody>
         </table>
     </div>
+    <br>
+    <h3>รายละเอียดการอบรม</h3>
+    <ul>
+        <li>วันที่: xxxx</li>
+        <li>เวลา: xxxx</li>
+    </ul>
     
     {{-- <p>รายการสั่งซื้อของคุณสำเร็จแล้ว</p>
     เรียนคุณ{{ $transaction->name }} {{ $transaction->lastname }}
