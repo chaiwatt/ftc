7615351431f8dab762ecb357a7407df1ad3675f8
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-12 14:48:36 +0700

    update some

--- app/Helper/EmailBox.php
+++ app/Helper/EmailBox.php
@@ -18,6 +18,7 @@ class EmailBox
             'title' => $title,
             'message' => $message,
             'type' => $type,
+            'transaction' => $transaction,
             ];
 
             Mail::to($mailto)->send(new FTCMail($data));
