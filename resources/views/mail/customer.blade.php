3e27597417ba70256c9f135636e141e1cf3e4f72
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-12 14:57:53 +0700

    update some

--- resources/views/mail/customer.blade.php
+++ resources/views/mail/customer.blade.php
@@ -1,4 +1,4 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,311 +9,47 @@
 <body>
     <h1>{{ $mailData['title'] }}</h1>
     <p>{{ $mailData['message'] }}</p>
-  
-    <p>รายการสั่งซื้อของคุณสำเร็จแล้ว</p>
+    <div>
+        <span class="text-sm text-grey-m2 align-middle">ลูกค้า:</span>
+        <span class="text-600 text-110 text-blue align-middle">ชัยวัฒน์ ทวีจันทร์</span>
+    </div>
+    <hr>
+    <div class="text-grey-m2">
+        <div>
+            ที่อยู่: 105 หมู่ 8 ต.เหมืองง่า อ.เมือง จ.ลำพูน 51000
+        </div>
+        <div>
+            อีเมล: joerocknpc@gmail.com
+        </div>
+        <div><b>088-2514838</b></div>
+        <hr>
+        <table>
+            <th>
+                <tr>
+                    <td>รายการ</td>
+                    <td>จำนวน</td>
+                    <td>ราคา</td>
+                </tr>
+            </th>
+            <tbody>
+                <tr>
+                    <td>อบรมคอร์ส Full-Stack developper</td>
+                    <td>2</td>
+                    <td>3000</td>
+                </tr>
+                <tr>
+                    <td>โดเมน</td>
+                    <td>1</td>
+                    <td>0</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    {{-- <p>รายการสั่งซื้อของคุณสำเร็จแล้ว</p>
     เรียนคุณ{{ $transaction->name }} {{ $transaction->lastname }}
-    รายการสั่งซื้อของคุณเสร็จสมบูรญ์แล้วค่ะ 
+    รายการสั่งซื้อของคุณเสร็จสมบูรญ์แล้วค่ะ  --}}
      
 </body>
-</html> --}}
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Invoice</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-
-<div class="page-content container">
-    <div class="page-header text-blue-d2">
-        <h1 class="page-title text-secondary-d1">
-            Invoice
-            <small class="page-info">
-                <i class="fa fa-angle-double-right text-80"></i>
-                ID: #111-222
-            </small>
-        </h1>
-
-        <div class="page-tools">
-            <div class="action-buttons">
-                <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="Print">
-                    <i class="mr-1 fa fa-print text-primary-m1 text-120 w-2"></i>
-                    พิมพ์
-                </a>
-                <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="PDF">
-                    <i class="mr-1 fa fa-file-pdf-o text-danger-m1 text-120 w-2"></i>
-                    ส่งออก
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <div class="container px-0">
-        <div class="row mt-4">
-            <div class="col-12 col-lg-12">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="text-center text-150">
-                            <!-- <i class="fa fa-book fa-2x text-success-m2 mr-1"></i> -->
-                            <span class="text-default-d3">Full-Stack Training Class</span>
-                        </div>
-                    </div>
-                </div>
-                <!-- .row -->
-
-                <hr class="row brc-default-l1 mx-n1 mb-4" />
-
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div>
-                            <span class="text-sm text-grey-m2 align-middle">ลูกค้า:</span>
-                            <span class="text-600 text-110 text-blue align-middle">ชัยวัฒน์ ทวีจันทร์</span>
-                        </div>
-                        <div class="text-grey-m2">
-                            <div class="my-1">
-                                ที่อยู่: 105 หมู่ 8 ต.เหมืองง่า อ.เมือง จ.ลำพูน 51000
-                            </div>
-                            <div class="my-1">
-                                อีเมล: joerocknpc@gmail.com
-                            </div>
-                            <div class="my-1"><i class="fa fa-phone fa-flip-horizontal text-secondary"></i> <b class="text-600">088-2514838</b></div>
-                        </div>
-                    </div>
-                    <!-- /.col -->
-
-                    <div class="text-95 col-sm-6 align-self-start d-sm-flex justify-content-end">
-                        <hr class="d-sm-none" />
-                        <div class="text-grey-m2">
-                            <div class="mt-1 mb-2 text-secondary-m1 text-600 text-125">
-                                Invoice
-                            </div>
-
-                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">ID:</span> #111-222</div>
-
-                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">วันที่:</span> Oct 12, 2019</div>
-
-                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">สถานะ:</span> <span class="badge badge-success badge-pill px-25">ชำระแล้ว</span></div>
-                        </div>
-                    </div>
-                    <!-- /.col -->
-                </div>
-
-                <div class="mt-4">
-                    <div class="row text-600 text-white bgc-default-tp1 py-25">
-                        <div class="d-none d-sm-block col-1">#</div>
-                        <div class="col-9 col-sm-5">รายการ</div>
-                        <div class="d-none d-sm-block col-4 col-sm-2">จำนวน</div>
-                        <div class="d-none d-sm-block col-sm-2">ราคา</div>
-                        <div class="col-2">ราคารวม</div>
-                    </div>
-
-                    <div class="text-95 text-secondary-d3">
-                        <div class="row mb-2 mb-sm-0 py-25">
-                            <div class="d-none d-sm-block col-1">1</div>
-                            <div class="col-9 col-sm-5">อบรมคอร์ส Full-Stack developper</div>
-                            <div class="d-none d-sm-block col-2">2</div>
-                            <div class="d-none d-sm-block col-2 text-95">15000</div>
-                            <div class="col-2 text-secondary-d2">3000</div>
-                        </div>
-
-                        <div class="row mb-2 mb-sm-0 py-25 bgc-default-l4">
-                            <div class="d-none d-sm-block col-1">2</div>
-                            <div class="col-9 col-sm-5">โดเมน</div>
-                            <div class="d-none d-sm-block col-2">1</div>
-                            <div class="d-none d-sm-block col-2 text-95">0</div>
-                            <div class="col-2 text-secondary-d2">0</div>
-                        </div>
-
-                    </div>
-
-                    <div class="row border-b-2 brc-default-l2"></div>
-
-            
-           
-
-                    <div class="row mt-3">
-                        <div class="col-12 col-sm-7 text-grey-d2 text-95 mt-2 mt-lg-0">
-                            รายการทั้งหมดได้รับการชำระเสร็จสิ้นแล้ว รวม
-                        </div>
-
-                        <div class="col-12 col-sm-5 text-grey text-90 order-first order-sm-last">
-                            <div class="row my-2">
-                                <div class="col-7 text-right">
-                                    รวม
-                                </div>
-                                <div class="col-5">
-                                    <span class="text-120 text-secondary-d1">28037.38</span>
-                                </div>
-                            </div>
-
-                            <div class="row my-2">
-                                <div class="col-7 text-right">
-                                    ภาษี 7%
-                                </div>
-                                <div class="col-5">
-                                    <span class="text-110 text-secondary-d1">1962.62</span>
-                                </div>
-                            </div>
-
-                            <div class="row my-2">
-                                <div class="col-7 text-right">
-                                    ส่วนลด
-                                </div>
-                                <div class="col-5">
-                                    <span class="text-110 text-secondary-d1">0</span>
-                                </div>
-                            </div>
-
-                            <div class="row my-2 align-items-center bgc-primary-l3 p-2">
-                                <div class="col-7 text-right">
-                                    รวมทั้งสิ้น
-                                </div>
-                                <div class="col-5">
-                                    <span class="text-150 text-success-d3 opacity-2">30000</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <hr />
-
-                
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<style type="text/css">
-body{
-    margin-top:20px;
-    color: #484b51;
-}
-.text-secondary-d1 {
-    color: #728299!important;
-}
-.page-header {
-    margin: 0 0 1rem;
-    padding-bottom: 1rem;
-    padding-top: .5rem;
-    border-bottom: 1px dotted #e2e2e2;
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-pack: justify;
-    justify-content: space-between;
-    -ms-flex-align: center;
-    align-items: center;
-}
-.page-title {
-    padding: 0;
-    margin: 0;
-    font-size: 1.75rem;
-    font-weight: 300;
-}
-.brc-default-l1 {
-    border-color: #dce9f0!important;
-}
-
-.ml-n1, .mx-n1 {
-    margin-left: -.25rem!important;
-}
-.mr-n1, .mx-n1 {
-    margin-right: -.25rem!important;
-}
-.mb-4, .my-4 {
-    margin-bottom: 1.5rem!important;
-}
-
-hr {
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-    border: 0;
-    border-top: 1px solid rgba(0,0,0,.1);
-}
-
-.text-grey-m2 {
-    color: #888a8d!important;
-}
-
-.text-success-m2 {
-    color: #86bd68!important;
-}
-
-.font-bolder, .text-600 {
-    font-weight: 600!important;
-}
-
-.text-110 {
-    font-size: 110%!important;
-}
-.text-blue {
-    color: #478fcc!important;
-}
-.pb-25, .py-25 {
-    padding-bottom: .75rem!important;
-}
-
-.pt-25, .py-25 {
-    padding-top: .75rem!important;
-}
-.bgc-default-tp1 {
-    background-color: rgba(121,169,197,.92)!important;
-}
-.bgc-default-l4, .bgc-h-default-l4:hover {
-    background-color: #f3f8fa!important;
-}
-.page-header .page-tools {
-    -ms-flex-item-align: end;
-    align-self: flex-end;
-}
-
-.btn-light {
-    color: #757984;
-    background-color: #f5f6f9;
-    border-color: #dddfe4;
-}
-.w-2 {
-    width: 1rem;
-}
-
-.text-120 {
-    font-size: 120%!important;
-}
-.text-primary-m1 {
-    color: #4087d4!important;
-}
-
-.text-danger-m1 {
-    color: #dd4949!important;
-}
-.text-blue-m2 {
-    color: #68a3d5!important;
-}
-.text-150 {
-    font-size: 150%!important;
-}
-.text-60 {
-    font-size: 60%!important;
-}
-.text-grey-m1 {
-    color: #7b7d81!important;
-}
-.align-bottom {
-    vertical-align: bottom!important;
-}
-
-</style>
-
-<script type="text/javascript">
-
-</script>
-</body>
 </html>
+
