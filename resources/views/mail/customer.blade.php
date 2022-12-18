a48bb2da21ed882ff2ea858458558910a411baf4
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-18 15:25:27 +0700

    update some

--- resources/views/mail/customer.blade.php
+++ resources/views/mail/customer.blade.php
@@ -1,6 +1,13 @@
+
+@component('mail::message')
+#Introduction
+
+The body of your message.
 @component('mail::table')
 | Laravel       | Table         | Example  |
 | ------------- |:-------------:| --------:|
 | Col 2 is      | Centered      | $10      |
 | Col 3 is      | Right-Aligned | $20      |
+@endcomponent
+
 @endcomponent
