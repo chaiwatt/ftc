886a4a3e250cabc3eb30c7cbc5ace44baefc08d9
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-18 17:13:51 +0700

    update some

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -458,23 +458,23 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <p>ชื่อ</p>
-                            <input id="name" name="name" placeholder="ชื่อ" type="text" class="input-text" required value="ชัยวัฒน์">
+                            <input id="name" name="name" placeholder="ชื่อ" type="text" class="input-text" required value="">
                         </div>
                         <div class="col-lg-6">
                             <p>นามสกุล</p>
-                            <input id="lastname" name="lastname" placeholder="นามสกุล" type="text" class="input-text" required value="ทวีจันทร์">
+                            <input id="lastname" name="lastname" placeholder="นามสกุล" type="text" class="input-text" required value="">
                         </div>
                         <div class="col-lg-6">
                             <p>อีเมล</p>
-                            <input id="email" name="email" placeholder="อีเมล" type="email" class="input-text" required value="joerocknpc@gmail.com" >
+                            <input id="email" name="email" placeholder="อีเมล" type="email" class="input-text" required value="" >
                         </div>
                         <div class="col-lg-6">
                             <p>โทรศัพท์</p>
-                            <input id="phone" name="phone" placeholder="โทรศัพท์" type="text" class="input-text" required value="0882514215">
+                            <input id="phone" name="phone" placeholder="โทรศัพท์" type="text" class="input-text" required value="">
                         </div>
                         <div class="col-lg-12">
                             <p>ที่อยู่</p>
-                            <textarea id="address" name="address" class="input-textarea" placeholder="ที่อยู่">105 หมู่8</textarea>
+                            <textarea id="address" name="address" class="input-textarea" placeholder="ที่อยู่"></textarea>
                         </div>
                         <div class="col-lg-6">
                             <p>จำนวนผู้เข้าเรียน</p>
