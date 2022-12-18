e7a20f6d111b220fd941d086a38a7213b74cae12
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-18 21:11:47 +0700

    update some

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Full-Stack Training Class'),
 
     /*
     |--------------------------------------------------------------------------
