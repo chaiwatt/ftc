dda70d02ec7e473f4eed756a3a4e041f318ae4b3
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2022-12-19 09:51:47 +0700

    update some

--- resources/views/mail/customer.blade.php
+++ resources/views/mail/customer.blade.php
@@ -1,10 +1,10 @@
 @component('mail::message')
 
-@if ($package['sourceinfo']['status'] == 'pending')
+{{-- @if ($package['sourceinfo']['status'] == 'pending')
         <b>{{$package['title']}}</b><br>
     @elseif($package['sourceinfo']['status'] == 'successful')
         <b>รายละเอียดการสั่งซื้อ</b><br>
-@endif
+@endif --}}
 
 ขอขอบพระคุณ,<br>
 Full-Stack Training Class
